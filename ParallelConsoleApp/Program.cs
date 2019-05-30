a0c5a1f3b878e4273d1b417a4b2ad34b341745cf
tjubuntu <tjubuntu@foxmail.com>
2019-05-30 23:06:09 +0800

    项目初始化

---
+++ ParallelConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
